ce828c4407cd1e372b39f18157e7934e1a271e97
Patrick Vogt <patrick.vogt@unibas.ch>
2025-06-19 13:47:09 +0200

    fix GB labeling

--- pkg/unit/bytes.go
+++ pkg/unit/bytes.go
@@ -11,14 +11,14 @@ var (
 func FormatGB(d any) string {
 	i, ok := d.(uint64)
 	if !ok {
-		return fmt.Sprintf("%v %T", d, d)
+		return fmt.Sprintf("%v-%T", d, d)
 	}
 	f := float64(i)
 	if f > gb {
-		return fmt.Sprintf("%.0f GB", f/gb)
+		return fmt.Sprintf("%.0fGB", f/gb)
 	}
 	if f > mb {
-		return fmt.Sprintf("%.0f MB", f/mb)
+		return fmt.Sprintf("%.0fMB", f/mb)
 	}
-	return fmt.Sprintf("%.0f KB", f/kb)
+	return fmt.Sprintf("%.0fKB", f/kb)
 }
