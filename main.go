a5f0546ad651640aee8f67b544ab82c026189487
Patrick Vogt <patrick.vogt@unibas.ch>
2025-06-19 16:12:47 +0200

    move root command to go-icinga

--- main.go
+++ main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
-	"github.com/vogtp/go-system-check/cmd/root"
+	"fmt"
+
+	"github.com/vogtp/go-icinga/pkg/check"
+	"github.com/vogtp/go-icinga/pkg/icinga"
+	"github.com/vogtp/go-system-check/cmd/cpu"
+	"github.com/vogtp/go-system-check/cmd/disk"
+	"github.com/vogtp/go-system-check/cmd/hashcmd"
+	"github.com/vogtp/go-system-check/cmd/memory"
+	"github.com/vogtp/go-system-check/cmd/systemdcmd"
 )
 
 func main() {
-	root.Command()
+	rootCtl := &check.Command{
+		Use:            "syscheck",
+		Short:          "Selfcontained icinga system checks",
+		NamePrefix:     "293",
+		DescriptionURL: "https://github.com/vogtp/go-system-check",
+		Criticality:    icinga.Criticality7x24,
+	}
+	rootCtl.AddCommand(cpu.Command())
+	// rootCtl.AddCommand(testcmd.Command())
+	rootCtl.AddCommand(hashcmd.Command())
+	rootCtl.AddCommand(memory.Command())
+	rootCtl.AddCommand(disk.Command())
+	rootCtl.AddCommand(systemdcmd.Command())
+
+	if err := rootCtl.Execute(); err != nil {
+		fmt.Println(err)
+	}
 }
